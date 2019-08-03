38d7f9a60f53b3f068552db616736e0e86e7ebb7
Guiorgy <Guiorgy123@gmail.com>
2019-08-03 10:21:00 +0000

    reformat output xml

--- main.py
+++ main.py
@@ -41,9 +41,13 @@ links = crawler.start()
 
 # write into file
 with open(args.output, 'w') as file:
-    file.write('<?xml version="1.0" encoding="UTF-8"?>\n\t<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">')
+    file.write('''<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+            xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+            xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">''')
 
     for link in links:
-        file.write('\n\t\t<url>\n\t\t\t<loc>\n\t\t\t\t{0}\n\t\t\t</loc>\n\t\t</url>'.format(link))
+        file.write('\n\t\t<url>\n\t\t\t<loc>{0}</loc>\n\t\t</url>'.format(link))
 
     file.write('</urlset>')
